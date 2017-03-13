b4fb413f213ebfbc2e5a19eb2721cdb50e2694c0
vaxo-m <m.vaxo@yahoo.com>
2017-03-13 15:23:45 +0400

    Header v 1.0

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// console.log("hahaaa maza faqa");
+// window.onload = function(){
+//   console.log("this is your maza faqa bitch!");
+// }
+//
+// document.getElementById("header").onload = function(){
+//   console.log("yes yes yes motherfucker");
+// }
+
+
+setTimeout(function(){ displayProfilePicture() }, 3000);
+
+function displayProfilePicture(){
+  document.getElementById("my-pic").style.cssText="opacity:1; display:block";
+  console.log("shedis aq eschemisa");
+}
