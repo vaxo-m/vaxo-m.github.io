e626077b7fdd58aa235a0f324bbc45ef18bc8b38
vaxo-m <m.vaxo@yahoo.com>
2017-03-15 20:58:27 +0400

    finalyzing the project

--- js/index.js
+++ js/index.js
@@ -8,8 +8,40 @@
 // }
 
 
-setTimeout(function(){ displayProfilePicture() }, 3000);
+setTimeout(function(){ displayProfilePicture() }, 3200);
 
 function displayProfilePicture(){
   document.getElementById("my-pic").style.cssText="opacity:1; display:block";
 }
+
+
+
+
+document.getElementById("copy-info").innerHTML = "Click to copy E-mail address";
+
+document.getElementById("mail").addEventListener("click", function(){
+  document.getElementById("copy-info").innerHTML = "E-mail address copied!";
+  document.getElementById("copy-email").style.cssText="display:block";
+  document.getElementById("copy-info").style.backgroundColor = "#afa";
+  setTimeout(function(){ clickAgain() }, 3000);
+});
+
+function clickAgain(){
+  document.getElementById("copy-info").innerHTML = "Click to copy E-mail address";
+  document.getElementById("copy-email").style.cssText="display:none";
+}
+
+
+
+var copyTextareaBtn = document.querySelector('.mail');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  var copyTextarea = document.querySelector('.js-copytextarea');
+  copyTextarea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+  } catch (err) {
+  }
+});
