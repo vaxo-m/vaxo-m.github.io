32f63635334dc3dce9a000d1262b3921f573f8d3
vaxo-m <m.vaxo@yahoo.com>
2017-03-17 19:30:13 +0400

    finalyzing website v2.0

--- js/index.js
+++ js/index.js
@@ -14,16 +14,13 @@ function displayProfilePicture(){
   document.getElementById("my-pic").style.cssText="opacity:1; display:block";
 }
 
-
-
-
 document.getElementById("copy-info").innerHTML = "Click to copy E-mail address";
 
 document.getElementById("mail").addEventListener("click", function(){
   document.getElementById("copy-info").innerHTML = "E-mail address copied!";
   document.getElementById("copy-email").style.cssText="display:block";
   document.getElementById("copy-info").style.backgroundColor = "#afa";
-  setTimeout(function(){ clickAgain() }, 3000);
+  setTimeout(function(){ clickAgain() }, 2400);
 });
 
 function clickAgain(){
@@ -31,10 +28,7 @@ function clickAgain(){
   document.getElementById("copy-email").style.cssText="display:none";
 }
 
-
-
 var copyTextareaBtn = document.querySelector('.mail');
-
 copyTextareaBtn.addEventListener('click', function(event) {
   var copyTextarea = document.querySelector('.js-copytextarea');
   copyTextarea.select();
@@ -44,4 +38,12 @@ copyTextareaBtn.addEventListener('click', function(event) {
     var msg = successful ? 'successful' : 'unsuccessful';
   } catch (err) {
   }
+});
+
+
+$(window).on('load', function() {
+	$('body').children().not('#animation-body').show();
+	$('#animation-body').hide();
+	$('body').css({ 'background-color': "#efefef"});
+
 });
